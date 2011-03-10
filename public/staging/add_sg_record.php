380615db053e7d27403b3d2f0bfc189fbc295f58
mager <andrew.mager@gmail.com>
2011-03-10 15:24:13 -0800

    housecleaning

--- public/staging/add_sg_record.php
+++ public/staging/add_sg_record.php
@@ -1,8 +1,9 @@
 <?php
-/* Store the handle and topic in SimpleGeo Storage */
 
+/* Store the handle and topic in SimpleGeo Storage */
 set_include_path(get_include_path() . PATH_SEPARATOR . '/home/amager/.pear/usr/local/php5/lib/pear/');
 require_once '../Services_SimpleGeo/Services/SimpleGeo.php';
+
 $client = new Services_SimpleGeo('ZL3NUaaTPF7DdvvsZpGD6wqvkBLWPDAs', 'MpCnGhagXDG7ppxQJvhupBRuewUmEHJh');
 $record = new Services_SimpleGeo_Record('net.publictext.topics', $pageID, $lat, $lon);
 $record->name = $topic;
